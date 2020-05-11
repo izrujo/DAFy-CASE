d3bf9a7970017ed8eac4a69e94b220047f4ed09c
찬형 김 <kch7990@gmail.com>
2020-05-11 17:16:00 +0900

    플래시 윈도우 관련 스테이징

---
+++ FlowChartEditorQt/x64/Debug/uic/ui_EditorIntroForm.h
@@ -0,0 +1,48 @@
+/********************************************************************************
+** Form generated from reading UI file 'EditorIntroForm.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_EDITORINTROFORM_H
+#define UI_EDITORINTROFORM_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+
+namespace EditorIntroFormUi {
+
+class Ui_EditorIntroFormClass
+{
+public:
+
+    void setupUi(QWidget *EditorIntroFormUi__EditorIntroFormClass)
+    {
+        if (EditorIntroFormUi__EditorIntroFormClass->objectName().isEmpty())
+            EditorIntroFormUi__EditorIntroFormClass->setObjectName(QString::fromUtf8("EditorIntroFormUi__EditorIntroFormClass"));
+        EditorIntroFormUi__EditorIntroFormClass->resize(400, 300);
+
+        retranslateUi(EditorIntroFormUi__EditorIntroFormClass);
+
+        QMetaObject::connectSlotsByName(EditorIntroFormUi__EditorIntroFormClass);
+    } // setupUi
+
+    void retranslateUi(QWidget *EditorIntroFormUi__EditorIntroFormClass)
+    {
+        EditorIntroFormUi__EditorIntroFormClass->setWindowTitle(QCoreApplication::translate("EditorIntroFormUi::EditorIntroFormClass", "Form", nullptr));
+    } // retranslateUi
+
+};
+
+} // namespace EditorIntroFormUi
+
+namespace EditorIntroFormUi {
+namespace Ui {
+    class EditorIntroFormClass: public Ui_EditorIntroFormClass {};
+} // namespace Ui
+} // namespace EditorIntroFormUi
+
+#endif // UI_EDITORINTROFORM_H
